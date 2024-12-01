30a7de6ad6781ef4bed726c69d7ed3c8d49c9c04
John Reid <john@noizebox.co.uk>
2024-12-01 05:31:29 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
         val pairs = input.map { it.split("   ") }
@@ -5,7 +7,7 @@ fun main() {
         val left = pairs.map { it[0].toInt() }.sorted()
         val right = pairs.map { it[1].toInt() }.sorted()
 
-        val apart = left.zip(right){ l, r -> Math.abs(l - r) }
+        val apart = left.zip(right){ l, r -> abs(l - r) }
         return apart.sum()
     }
 
