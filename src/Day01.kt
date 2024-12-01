752541f738405e36caf1212616a7ee28db61e59a
John Reid <john@noizebox.co.uk>
2024-12-01 05:30:28 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val pairs = input.map { it.split("   ") }
+
+        val left = pairs.map { it[0].toInt() }.sorted()
+        val right = pairs.map { it[1].toInt() }.sorted()
+
+        val apart = left.zip(right){ l, r -> Math.abs(l - r) }
+        return apart.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val pairs = input.map { it.split("   ") }
+
+        val left = pairs.map { it[0].toInt() }
+        val right = pairs.map { it[1].toInt() }
+
+        val similarity = left.zip(right){ l, r -> l * right.count { it == l }}
+        return similarity.sum()
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("1   2")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
